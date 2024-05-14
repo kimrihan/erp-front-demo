75518ae0c2a168b978a5553667100e2d3da54e6f
kimrihan <rihan.ringo@gmail.com>
2024-05-14 12:16:18 +0900

    add managing form state

--- src/customers/pages/NewCustomer.jsx
+++ src/customers/pages/NewCustomer.jsx
@@ -1,17 +1,81 @@
-import Input from "../../shared/components/FormElements/Input";
+import { useCallback, useReducer } from "react";
+
+import Input from "../../shared/components/FormElements/Input.jsx";
+import Button from "../../shared/components/FormElements/Button.jsx";
+
 import { VALIDATOR_REQUIRE } from "../../shared/util/validators.js";
 import "./NewCustomer.css";
 
+const formReducer = (state, action) => {
+  let formIsValid;
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 const NewCustomer = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      identification_number: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
   return (
     <form className="customer-form">
       <Input
+        id="name"
         element="input"
         type="text"
         label="고객명"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="입력이 바르지 않습니다."
+        onInput={inputHandler}
       />
+      <Input
+        id="identification_number"
+        element="input"
+        type="text"
+        label="주민번호"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="입력이 바르지 않습니다."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        고객 등록
+      </Button>
     </form>
   );
 };
--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 
 import { validate } from "../../util/validators";
 
@@ -28,6 +28,13 @@ const Input = (props) => {
     isTouched: false,
     isValid: false,
   });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
 
   const changeHandler = (event) => {
     dispatch({
